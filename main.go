6c76c671c248a6e3e464104ad739a94954fad4c1
Artemides <edmpulasky@gmail.com>
2023-07-08 01:07:08 -0500

    expand api routes

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"log"
 
+	"github.com/Artemides/go-fiber-api/controllers"
 	"github.com/Artemides/go-fiber-api/initializers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func init() {
@@ -13,13 +16,32 @@ func init() {
 
 func main() {
 	app := fiber.New()
+	micro := fiber.New()
 
-	app.Get("/api/greetings", func(c *fiber.Ctx) error {
+	app.Mount("/api", micro)
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:300",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowMethods:     "GET, POST, PATCH, DELETE",
+		AllowCredentials: true,
+	}))
+
+	micro.Route("/notes", func(router fiber.Router) {
+		router.Get("", controllers.FindNotes)
+		router.Post("/", controllers.CreateNoteHandler)
+	})
+	micro.Route("/note/:noteId", func(router fiber.Router) {
+		router.Get("", controllers.FindNote)
+		router.Patch("", controllers.UpdateNote)
+		router.Delete("", controllers.DeleteNote)
+	})
+
+	micro.Get("/api/greetings", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{
 			"status":  "alles ok",
 			"message": "Welcome",
 		})
 	})
-
 	log.Fatal(app.Listen(":4000"))
 }
