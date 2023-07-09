465574308de5bf6c0e7b3f975d9c1fa0099a94f2
Artemides <edmpulasky@gmail.com>
2023-07-08 20:40:57 -0500

    load env vars config

--- initializers/db.go
+++ initializers/db.go
@@ -5,12 +5,23 @@ import (
 	"os"
 
 	"github.com/Artemides/go-fiber-api/models"
+	"github.com/spf13/viper"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
+
+type Config struct {
+	DBHost         string `mapstructure:"MYSQL_HOST"`
+	DBUsername     string `mapstructure:"MSQL_USER"`
+	DBUserPassword string `mapstructure:"MYSQL_PASSWORD"`
+	DBName         string `mapstructure:"MUSQL_DB"`
+	DBPort         string `mapstructure:"MYSQL_PORT"`
+
+	ClientOrigin string `mapstrcuture:"CLIENT_ORIGIN"`
+}
 
 func ConnectDB() {
 	var err error
@@ -27,4 +38,18 @@ func ConnectDB() {
 	DB.AutoMigrate(&models.Note{})
 	log.Println("🚀 Connection Successfully DB")
 
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("")
+
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
 }
